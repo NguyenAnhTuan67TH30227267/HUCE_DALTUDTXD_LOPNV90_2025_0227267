64506769481857c625b61b177fef08c5b3ce1adc
NguyenAnhTuan67TH30227267 <0227267@st.huce.edu.vn>
2025-12-13 22:33:28 +0700

    NguyenAnhTuan67TH30227267/Update Model
    update ConcreteProperties, SteelProperties, UserAccount và UserModel

--- HUCE_DALTUDTXD_LOPNV90_2025_0227267/HUCE_DALTUDTXD_LOPNV90_2025_0227267/Models/UserModel.cs
+++ HUCE_DALTUDTXD_LOPNV90_2025_0227267/HUCE_DALTUDTXD_LOPNV90_2025_0227267/Models/UserModel.cs
@@ -8,6 +8,11 @@ namespace HUCE_DALTUDTXD_LOPNV90_2025_0227267.Models
 {
     public class UserModel
     {
-
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
